f13584a19d4dc0a38acbd1789a46a75e145c340f
manoj_nikam <manoj.nikam.aids.2023@vpkbiet.org>
2026-04-17 12:44:43 +0530

    Added program1

--- program1.py
+++ program1.py
@@ -1,6 +1,6 @@
 num = int(input("Enter a number: "))
 print("Multiplication Table of", num)
-for i in range(1, 11):
+for i in range(1, 21):
     print(num, "x", i, "=", num * i)
     num = int(input("Enter a number: "))
     print("Multiplication Table of", num)
